e52ebd176200027d05a669d8b9392740fcbcf21a
boomNDS <boomsixboom@gmail.com>
2019-02-19 19:18:14 +0700

    add vue.js

---
+++ src/components/index.vue
@@ -0,0 +1,10 @@
+<template>
+    <div class="index">
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Reprehenderit quia aliquid omnis maiores ullam sequi est non ipsum dignissimos doloremque, sint rem enim architecto labore laudantium! Obcaecati omnis harum odit?</p>
+    </div>
+</template>
+<script>
+export default {
+    name: 'index',
+}
+</script>
