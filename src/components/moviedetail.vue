340a31546f78fe58faccb30c6e6cdc31d18f1d06
Frong-nt <60070025@kmitl.ac.th>
2019-02-20 17:58:33 +0700

    moviedetail connect to data

--- src/components/moviedetail.vue
+++ src/components/moviedetail.vue
@@ -4,28 +4,41 @@
       <div class="columns notification">
         <div class="column">
           <div class="videoWrapper">
-            <iframe
-              width="560"
-              height="349"
-              src="http://www.youtube.com/embed/n_dZNLr2cME?rel=0&hd=1"
-              frameborder="0"
-              allowfullscreen
-            ></iframe>
+            <iframe width="560" height="349" :src="movie.video" frameborder="0" allowfullscreen></iframe>
           </div>
         </div>
         <div class="column">
-          <h1 class="title is-3">Movie</h1>
-          <p
-            class="content has-text-left is-small"
-          >Lorem, ipsum dolor sit amet consectetur adipisicing elit. Fuga cum minima corporis ea aut optio? Sequi reiciendis pariatur facilis id ullam? Beatae facilis placeat ipsum consectetur in corrupti numquam explicabo.</p>
+          <h1 class="title is-3">{{movie.name.th+" "}} ({{movie.name.en}})</h1>
+          <div class="has-text-left">
+            <p>
+              <strong>ผู้กำกับ:</strong>
+              <span v-for="director in movie.director">{{director}}</span>
+            </p>
+            <p>
+              <strong>นักแสดง:</strong>
+              <span v-for="actor in movie.actors">{{actor}}</span>
+            </p>
+            <p>
+              <strong>หมวดหมู่:</strong>
+              <span v-for="category in movie.categories">{{category+", "}}</span>
+            </p>
+          </div>
         </div>
       </div>
     </div>
   </div>
 </template>
 <script>
+import movies from "../assets/data/movies.js";
+
 export default {
-  name: "moviedetail"
+  name: "moviedetail",
+  data: function() {
+    return {
+      movies: movies,
+      movie: movies[0]
+    };
+  }
 };
 </script>
 <style scoped>
@@ -34,16 +47,16 @@ export default {
   padding: 5%;
 }
 .videoWrapper {
-	position: relative;
-	padding-bottom: 56.25%; /* 16:9 */
-	padding-top: 25px;
-	height: 0;
+  position: relative;
+  padding-bottom: 56.25%; /* 16:9 */
+  padding-top: 25px;
+  height: 0;
 }
 .videoWrapper iframe {
-	position: absolute;
-	top: 0;
-	left: 0;
-	width: 100%;
-	height: 100%;
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
 }
 </style>
